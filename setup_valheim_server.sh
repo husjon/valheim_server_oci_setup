742e4118db112dc36ccefc0850d2c7316192b4e8
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2022-12-14 20:15:29 +0100

    added validate to the steamcmd used for updating (thanks @HametAk)

--- setup_valheim_server.sh
+++ setup_valheim_server.sh
@@ -261,7 +261,7 @@ function main {
 		            +@sSteamCmdForcePlatformType linux \\
 		            +force_install_dir /home/ubuntu/valheim_server \\
 		            +login anonymous \\
-		            +app_update 896660 \\
+		            +app_update 896660 validate \\
 		            +quit
 
 		systemctl --user start valheim_server
